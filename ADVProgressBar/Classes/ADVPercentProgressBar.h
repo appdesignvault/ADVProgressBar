902d5801104054cd85dbcdc4488d89868ca86037
corerd <corerd@live.com>
2013-02-02 19:55:15 +0100

    Add oxygen.

--- ADVProgressBar/Classes/ADVPercentProgressBar.h
+++ ADVProgressBar/Classes/ADVPercentProgressBar.h
@@ -32,36 +32,36 @@
 */
 
 /*!
- ADVPercentProgressBar depicts the progress of a task over time.
+ Define a Custom UIView to depict the progress of a task over time.
  
- The amount of `progress` is displayed by a horizontal bar,
- called `progressImageView`, that represents a range of values.
- This range is controlled by the `minProgressValue` and the `maxProgressValue`
+ The amount of **progress** is displayed by a horizontal bar,
+ called **progressImageView**, that represents a range of values.
+ This range is controlled by the **minProgressValue** and the **maxProgressValue**
  properties, whose default values are 0.0 and 1.0 respectively.
  
- The user can customize the appearance of the `progressImageView` horizontal bar
- selecting its color in the `ADVProgressBarColor` set.
+ The user can customize the appearance of the **progressImageView** horizontal bar
+ selecting its color in the **ADVProgressBarColor** set.
  
- The current `progress` is represented by a floating-point value (default 0.0)
- between `minProgressValue` and `maxProgressValue`, inclusive;
+ The current **progress** is represented by a floating-point value (default 0.0)
+ between **minProgressValue** and **maxProgressValue**, inclusive;
  values less or greater are pinned to those limits.
  
- The current `progress` is shown by the position of a cursor, coupled with
- the `progressImageView` and called `percentView`; within its bounds, it is
+ The current **progress** is shown by the position of a cursor, coupled with
+ the **progressImageView** and called **percentView**; within its bounds, it is
  shown a textual indication of the amount of the task that has completed.
- The way this indication is displayed, is controlled by the `showPercentage`
- property: by default its value is `true` and the current `progress` is
+ The way this indication is displayed, is controlled by the **showPercent**
+ property: by default its value is `YES` and the current **progress** is
  formatted as a percent complete respect the range of values.
  
- The user can set to `false` the `showPercentage` property to display the integral
+ The user can set to `NO` the **showPercent** property to display the integral
  amount of the task that has completed.
- Since the width of the `progressImageView` cursor is fixed to 3 textual
- characters wide, if `maxProgressValue` is greater than 999, then the current
- `progress` is formatted as a percent even if the `showPercentage` property
- is set to `false`.
+ Since the width of the **progressImageView** cursor is fixed to 3 textual
+ characters wide, if **maxProgressValue** is greater than 999, then the current
+ **progress** is formatted as a percent even if the **showPercent** property
+ is set to `NO`.
  
- The `showPercentage` property isn't taken in account also when the
- `maxProgressValue` is less or equal to 1.0: the current `progress` will be
+ The **showPercent** property isn't taken in account also when the
+ **maxProgressValue** is less or equal to 1.0: the current **progress** will be
  always formatted as a percent.
  */
 
@@ -72,7 +72,7 @@
 
 // Types
 
-/// Set of `progressImageView` horizontal bar colors.
+/// Set of **progressImageView** horizontal bar colors.
 typedef enum
 {
     ADVProgressBarGreen,
@@ -86,10 +86,10 @@ typedef enum
 /// The current amount of the task that has completed (default 0.0).
 @property (nonatomic, readwrite, assign) CGFloat progress;
 
-/// Minimum in the range of `progress` values (default 0.0).
+/// Minimum in the range of **progress** values (default 0.0).
 @property (nonatomic, readwrite, assign) CGFloat minProgressValue;
 
-/// Maximum in the range of `progress` values (default 1.0).
+/// Maximum in the range of **progress** values (default 1.0).
 @property (nonatomic, readwrite, assign) CGFloat maxProgressValue;
 
 /// Toggle between percent or integral view of current **progress** (default `YES` = percent)
@@ -101,14 +101,14 @@ typedef enum
  Initializes and returns a newly allocated `ADVPercentProgressBar` view object.
  
  The view is allocated with the specified frame rectangle and
- the `progressImageView` horizontal bar color.
+ the **progressImageView** horizontal bar color.
  
  @param frame
     The frame rectangle for the view, measured in points.
     The origin of the frame is relative to the superview in which you plan to add it.
  
  @param barColor
-    The `progressImageView` horizontal bar color in the `ADVProgressBarColor` set.
+    The **progressImageView** horizontal bar color in the **ADVProgressBarColor** set.
  
  @return
     An initialized `ADVPercentProgressBar` view object
